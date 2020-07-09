9c505ecce82b425255c32867ef17ac95c1ed01b6
shivomkaras@gmail.com <Steria.2848>
2020-07-09 16:34:03 +0530

    minor change

--- src/apis/CategoryApi.js
+++ src/apis/CategoryApi.js
@@ -1,7 +1,7 @@
 import {API_BASE_URL} from '../config/Variables'
 import ApiHandler from "./ApiHandler";
 
-let CategoryApi ={
+let CategoryApi ={ 
     getAllCategory:(callback, errorcallback, searchText="", sortBy="id", pageNo=0, pageSize=10)=>{
         let url=`${API_BASE_URL}category/getall?searchText=${searchText}&pageNo=${pageNo}&pageSize=${pageSize}&sortBy=${sortBy}`;
         ApiHandler.getRequest(url, callback, errorcallback);
